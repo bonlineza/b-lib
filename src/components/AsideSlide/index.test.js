2cc543a1a1326c08d24512a51b0e480671f8814e
Sebastian Scholle <sscholle@gmail.com>
2019-07-11 11:55:37 +0200

    Upgraded a few packages to fix the tests

--- src/components/AsideSlide/index.test.js
+++ src/components/AsideSlide/index.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import 'test-util/setup';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import AsideSlide from './index';
 
 it('should render correctly <AsideSlide>', () => {
-  const wrapper = mount(
+  const wrapper = shallow(
     <AsideSlide title="Basic Aside Slide test" isOpen toggle={() => null}>
       <div>Some child element</div>
     </AsideSlide>,
