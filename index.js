ed6e2904ea0396fb1f7a1ac58c853d86e095e155
Sebastian Scholle <sscholle@gmail.com>
2019-07-12 10:51:19 +0200

    Move modules to 'dev'
    Add exports for Components

---
+++ index.js
@@ -0,0 +1,7 @@
+import AsideSlide from './src/components/AsideSlide';
+import DropOptions from './src/components/DropOptions';
+import Overlay from './src/components/Overlay';
+
+exports.AsideSlide = AsideSlide;
+exports.DropOptions = DropOptions;
+exports.Overlay = Overlay;
