366ea66631c8deadbce98a27f16bf30b3380d03c
Phillip Ruhesi <pruhesi@gmail.com>
2019-07-18 17:48:31 +0200

    Added ToolTip stories
    Added ToolTip stories

---
+++ stories/ToolTip.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { wrapTooltip } from 'components/ToolTip';
+
+const WrappedTextToolTip = () => (
+  <Fragment>
+    <div className="hover-tooltip-container">
+      <span>{wrapTooltip('children', 'text')}</span>
+    </div>
+  </Fragment>
+);
+
+storiesOf('ToolTip', module).add('default', () => <WrappedTextToolTip />);
+
+const WrappedButtonToolTip = () => (
+  <Fragment>
+    <div className="hover-tooltip-container">
+      <span>
+        {wrapTooltip(
+          <button type="button" onClick={action('actionComponent action')}>
+            children
+          </button>,
+          'text',
+        )}
+      </span>
+    </div>
+  </Fragment>
+);
+
+storiesOf('ToolTip', module).add('ButtonToolTip', () => (
+  <WrappedButtonToolTip />
+));
