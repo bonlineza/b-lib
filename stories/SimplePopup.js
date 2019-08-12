04fa9247fb7a6d43e5e5673a619fc641a89bb50b
Musango <musangow@gmail.com>
2019-08-12 13:36:02 +0200

    SimplePopUp: added prop cannotOutsideClose
    if true, you cannot click the modal by click away outside the bounds of the modal
    
    Added a story for the prop cannotOutsideClose
    
    Updated content in story
    
    SimplePopup: added logic to start listener conditionally based on prop
    
    SimplePopup: refactored logic of running prop close
    
    stories/SimplePopup: made code more reusable

--- stories/SimplePopup.js
+++ stories/SimplePopup.js
@@ -2,52 +2,47 @@ import React, { Fragment, useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import SimplePopup from 'components/SimplePopup';
 import { action } from '@storybook/addon-actions';
-// import OverlayChild from 'test-util/OverlayChild';
 
-const DefaultSimplePopup = () => {
+const DefaultSimplePopup = (props = {}) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const testOptions = [
-    {
-      cb: () => {
-        action()('Cancel Called');
-        setIsOpen(false);
-      },
-      buttonText: 'Test Cancel Button',
-      buttonClass: 'cancel-button',
-      dataQeId: 'button-cancel',
+  const defaultProps = {
+    isOpen,
+    title: 'Test Title',
+    renderContent: () => (
+      <div className="popup-content">some div with content</div>
+    ),
+    close: () => {
+      action()('Close Action Called');
+      setIsOpen(false);
     },
-    {
-      cb: () => {
-        action()('Action Called');
-        setIsOpen(false);
+    options: [
+      {
+        cb: () => {
+          action()('Cancel Called');
+          setIsOpen(false);
+        },
+        buttonText: 'Test Cancel Button',
+        buttonClass: 'cancel-button',
+        dataQeId: 'button-cancel',
       },
-      buttonText: 'Test Action Button',
-      buttonClass: 'action-button',
-      dataQeId: 'button-submit',
-    },
-  ];
+    ],
+  };
+
+  const testProps = { ...defaultProps, ...props };
 
   return (
     <Fragment>
       <button type="button" onClick={() => setIsOpen(true)}>
         Open SimplePopup
       </button>
-      <SimplePopup
-        isOpen={isOpen}
-        title="Test Title"
-        close={() => {
-          action()('Close Action Called');
-          setIsOpen(false);
-        }}
-        renderLoader={() => <div className="popup-loader">Loading...</div>}
-        renderContent={() => (
-          <div className="popup-content">some div with content</div>
-        )}
-        options={testOptions}
-      />
+      <SimplePopup {...testProps} />
     </Fragment>
   );
 };
 
 storiesOf('SimplePopup', module).add('default', () => <DefaultSimplePopup />);
+
+storiesOf('SimplePopup', module).add('cannot close from outside', () => (
+  <DefaultSimplePopup cannotOutsideClickClose />
+));
