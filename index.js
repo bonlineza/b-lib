7978e285f80a80954079746db101cff00a1d5f1d
Sebastian Scholle <sscholle@gmail.com>
2019-08-14 16:46:35 +0200

    Add Map Component with tests
    Add Map Stories

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 import AsideSlide from './src/components/AsideSlide/index.js';
+import ButtonGroup from './src/components/ButtonGroup/index.js';
 import DropOptions from './src/components/DropOptions/index.js';
-import SwitchView from './src/components/SwitchView/index.js';
-import ToolTip from './src/components/ToolTip/index.js';
-import Overlay from './src/components/Overlay/index.js';
+import EllipsisLoader from './src/components/EllipsisLoader/index.js';
+import Map from './src/components/Map/index.js';
 import MobileSideMenu from './src/components/MobileSideMenu/index.js';
-import SimplePopup from './src/components/SimplePopup/index.js';
+import Overlay from './src/components/Overlay/index.js';
 import PageReady, {
   Context as PageReadyContext,
 } from './src/components/PageReady/index.js';
-import EllipsisLoader from './src/components/EllipsisLoader/index.js';
 import PDFPreview from './src/components/PDFPreview/index.js';
-import WYSIWYG from './src/components/WYSIWYG/index.js';
 import SelectRows from './src/components/SelectRows/index.js';
+import SimplePopup from './src/components/SimplePopup/index.js';
+import SwitchView from './src/components/SwitchView/index.js';
+import ToolTip from './src/components/ToolTip/index.js';
 import UntilReady from './src/components/UntilReady/index.js';
+import WYSIWYG from './src/components/WYSIWYG/index.js';
 
 export {
   AsideSlide,
+  ButtonGroup,
   DropOptions,
-  SwitchView,
-  ToolTip,
-  Overlay,
+  EllipsisLoader,
+  Map,
   MobileSideMenu,
-  SimplePopup,
+  Overlay,
   PageReady,
   PageReadyContext,
-  EllipsisLoader,
   PDFPreview,
-  WYSIWYG,
   SelectRows,
+  SimplePopup,
+  SwitchView,
+  ToolTip,
   UntilReady,
+  WYSIWYG,
 };
