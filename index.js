d89f7b40b0c876a7e4b0a2c4d501c0d21d7961c2
Musango <musangow@gmail.com>
2019-08-26 22:17:06 +0200

    Added MobileSideMenu to index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import AsideSlide from './src/components/AsideSlide/index.js';
 import DropOptions from './src/components/DropOptions/index.js';
 import SwitchView from './src/components/SwitchView/index.js';
 import Overlay from './src/components/Overlay/index.js';
+import MobileSideMenu from './src/components/MobileSideMenu/index.js';
 import SimplePopup from './src/components/SimplePopup/index.js';
 import PageReady, {
   Context as PageReadyContext,
@@ -17,4 +18,5 @@ export {
   PageReady,
   PageReadyContext,
   EllipsisLoader,
+  MobileSideMenu,
 };
