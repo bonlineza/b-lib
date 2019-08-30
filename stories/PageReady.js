7fede27faad22f34f0f70edb2515478a67d8e259
Musango <musangow@gmail.com>
2019-08-30 12:31:12 +0200

    stories/PageReady: Adjusted stories
    PageReady stories: refactored stories

--- stories/PageReady.js
+++ stories/PageReady.js
@@ -1,33 +1,34 @@
-import React, { Fragment, useState } from 'react';
-import moxios from 'moxios';
+import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
 import PageReady, { Context } from 'components/PageReady';
 
-moxios.install();
-const data = [
-  {
-    id: 1,
-    name: 'Clark Kent',
-  },
-  {
-    id: 2,
-    name: 'Diana Prince',
-  },
-  {
-    id: 3,
-    name: 'Harley Quinn',
-  },
-];
+const getSuccessRequestInstance = () =>
+  new Promise(resolve => {
+    setTimeout(function() {
+      resolve([
+        {
+          id: 1,
+          name: 'Clark Kent',
+        },
+        {
+          id: 2,
+          name: 'Diana Prince',
+        },
+        {
+          id: 3,
+          name: 'Harley Quinn',
+        },
+      ]);
+    }, 250);
+  });
 
 const getPageReady = props => (
   <PageReady {...props}>
     <Context.Consumer>
-      {({ data: names }) => (
+      {({ data }) => (
         <Fragment>
-          {names.map(({ name, id }) => (
-            <div className="list-item" key={id}>
-              {name}
-            </div>
+          {data.map(({ id, name }) => (
+            <div key={id}>{name}</div>
           ))}
         </Fragment>
       )}
@@ -35,26 +36,21 @@ const getPageReady = props => (
   </PageReady>
 );
 
-const DefaultPageReady = () => {
-  moxios.stubRequest('https://fake.test/names', {
-    status: 200,
-    response: data,
-  });
-  return getPageReady({ url: 'https://fake.test/names' });
-};
+const DefaultPageReady = () =>
+  getPageReady({ getRequestInstance: getSuccessRequestInstance });
 
 storiesOf('PageReady', module).add('default', () => <DefaultPageReady />);
 
-const FailedPageReady = () => {
-  moxios.stubRequest(/.*/, {
-    status: 400,
+const getFailedRequestInstance = () =>
+  fetch('https://local.test/546456').then(function(response) {
+    return response.json();
   });
 
-  return getPageReady({
-    url: 'https://fake.test/sdfdkfjhg',
-    customErrorMessage: 'This a custom error message',
+const FailedPageReady = () =>
+  getPageReady({
+    getRequestInstance: getFailedRequestInstance,
+    customErrorMessage: 'Custom error message',
   });
-};
 
 storiesOf('PageReady', module).add('custom error message', () => (
   <FailedPageReady />
