10e6ea1640b8d3b99d2e7b36ee7cbab39b1f7fbc
Musango <musangow@gmail.com>
2019-09-27 10:39:08 +0200

    Create MultiLineSelect and tests
    MultiSelect/index.js: Remove unchangeRaw

---
+++ src/components/MultiLineSelect/index.js
@@ -0,0 +1,120 @@
+import React, { Component, Fragment } from 'react';
+import SelectRows from 'components/SelectRows';
+
+/**
+ * Description
+ * show one or many rows that are removable - top one is always 'addable' in multi-mode
+ * add button triggers onAdd, remove button triggers onRemove
+ */
+
+type MultiLineSelectProps = {
+  selectorObj?: Object,
+  label: string,
+  placeholder: string,
+  viewOnly: boolean,
+  hasError: boolean,
+  errors?: [],
+  multiline: boolean,
+  onChange?: Function,
+  displayData?: null | [{ key: String, value: String }],
+  keyField?: string,
+  baseClassName?: string,
+};
+
+class MultiLineSelect extends Component {
+  constructor(props: MultiLineSelectProps) {
+    super(props);
+    this.state = {
+      selectListVisible: false,
+      selection: props.defaultValue ? [props.defaultValue] : [],
+    };
+  }
+
+  handleChange = rows => this.props.onChange(rows);
+
+  removeItem = key => {
+    this.setState(prevState => {
+      const keyIndex = prevState.selection.findIndex(
+        item => item[this.props.keyField] === key,
+      );
+      const newSelection = [...prevState.selection];
+      newSelection.splice(keyIndex, 1);
+      this.handleChange(newSelection);
+      return { ...prevState, selection: newSelection };
+    });
+  };
+
+  addItem = row => {
+    this.setState(prevState => {
+      const newSelection = [...prevState.selection].concat(row);
+      this.handleChange(newSelection);
+      return {
+        ...prevState,
+        selection: newSelection,
+      };
+    });
+  };
+
+  toggle = stateProp =>
+    this.setState(prevState => ({
+      ...prevState,
+      [stateProp]: !prevState[stateProp],
+    }));
+
+  showList = () => {
+    this.toggle('selectListVisible');
+  };
+
+  render = () => {
+    const {
+      selectorObj: { selector: Selector, selectorProps = {} },
+      placeholder,
+      multiline,
+      displayData,
+    } = this.props;
+
+    const combinedSelectorProps = {
+      ...selectorProps,
+      ...{
+        title: placeholder,
+        isSelectorActive: this.state.selectListVisible,
+        toggleSelector: () => this.toggle('selectListVisible'),
+        addItem: this.addItem,
+      },
+    };
+
+    return (
+      <Fragment>
+        <div className={this.props.baseClassName}>
+          <SelectRows
+            single={!multiline}
+            rows={
+              displayData ||
+              this.state.selection.map((item, key) => ({
+                key: `${this.props.keyField}-${key}`,
+                value: item,
+              }))
+            }
+            onAddClick={() => this.showList()}
+            onRemoveClick={key => this.removeItem(key)}
+            rowClassName={this.props.rowClassName}
+            placeholder={placeholder}
+          />
+        </div>
+        <Selector {...combinedSelectorProps} />
+      </Fragment>
+    );
+  };
+}
+
+MultiLineSelect.defaultProps = {
+  default: {},
+  multiline: false,
+  onChange: () => false,
+  displayData: null,
+  keyField: 'id',
+  baseClassName: 'multiline-select',
+  rowClassName: 'select-rows',
+};
+
+export default MultiLineSelect;
---
+++ src/components/MultiLineSelect/index.test.js
@@ -0,0 +1,69 @@
+import React, { Fragment } from 'react';
+import 'test-util/setup';
+import MultiLineSelect from 'components/MultiLineSelect/index';
+import { mount } from 'enzyme';
+
+const defaultMultiLineProps = {
+  label: 'Dummy Label',
+  placeholder: 'Add Item',
+  rowClassName: 'button-row',
+  hasError: false,
+  errors: [],
+  multiline: true,
+  viewOnly: false,
+  onChange: () => false,
+};
+
+const ItemSelector = ({ addItem = () => null }) => (
+  <Fragment>
+    <button id="item-choice-one" onClick={() => addItem('Product One')}>
+      Select Product One
+    </button>
+    <button id="item-choice-two" onClick={() => addItem('Product Two')}>
+      Select Product Two
+    </button>
+    <button id="item-choice-three" onClick={() => addItem('Product Three')}>
+      Select Product Three
+    </button>
+  </Fragment>
+);
+
+const WrappedMultiLineSelect = (props = {}) => {
+  const selectorObj = {
+    selector: ItemSelector,
+  };
+
+  const finalProps = {
+    ...defaultMultiLineProps,
+    ...props,
+    selectorObj,
+  };
+
+  return <MultiLineSelect {...finalProps} />;
+};
+
+describe('MultiSelect', () => {
+  it('Multiselect can add and remove an product one', () => {
+    const wrapper = mount(<WrappedMultiLineSelect />);
+    wrapper.find('#item-choice-one').simulate('click');
+    expect(wrapper.find('.button-row--value').text()).toBe('Product One');
+    wrapper.find('.button-row--value').simulate('click');
+    expect(wrapper.find('.button-row').length).toBe(1);
+  });
+
+  it('Multiselect can only add one product to list', () => {
+    const wrapper = mount(<WrappedMultiLineSelect multiline={false} />);
+
+    wrapper.find('#item-choice-one').simulate('click');
+    expect(wrapper.find('.button-row--value').text()).toBe('Product One');
+    wrapper.find('.button-row--value').simulate('click');
+
+    wrapper.find('#item-choice-two').simulate('click');
+    expect(wrapper.find('.button-row--value').text()).toBe('Product Two');
+    wrapper.find('.button-row--value').simulate('click');
+
+    wrapper.find('#item-choice-three').simulate('click');
+    expect(wrapper.find('.button-row--value').text()).toBe('Product Three');
+    wrapper.find('.button-row--value').simulate('click');
+  });
+});
