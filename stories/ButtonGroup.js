fa21fc869fb2c2b717d1840fd9e3f35b75052d18
Sebastian Scholle <sscholle@gmail.com>
2019-10-25 12:09:16 +0200

    Fix ButtonGroup story lint issues

--- stories/ButtonGroup.js
+++ stories/ButtonGroup.js
@@ -5,20 +5,36 @@ import ButtonGroup from 'components/ButtonGroup';
 
 const DefaultButtonGroup = () => (
   <ButtonGroup>
-    <button onClick={action('Button 1')}>Button 1</button>
-    <button onClick={action('Button 2')}>Button 2</button>
-    <button onClick={action('Button 3')}>Button 3</button>
-    <button onClick={action('Button 4')}>Button 4</button>
+    <button type="button" onClick={action('Button 1')}>
+      Button 1
+    </button>
+    <button type="button" onClick={action('Button 2')}>
+      Button 2
+    </button>
+    <button type="button" onClick={action('Button 3')}>
+      Button 3
+    </button>
+    <button type="button" onClick={action('Button 4')}>
+      Button 4
+    </button>
   </ButtonGroup>
 );
 storiesOf('ButtonGroup', module).add('default', () => <DefaultButtonGroup />);
 
 const ThreeAltButtonGroup = () => (
   <ButtonGroup alt numButtons={3}>
-    <button onClick={action('Button 1')}>Button 1</button>
-    <button onClick={action('Button 2')}>Button 2</button>
-    <button onClick={action('Button 3')}>Button 3</button>
-    <button onClick={action('Button 4')}>Button 4</button>
+    <button type="button" onClick={action('Button 1')}>
+      Button 1
+    </button>
+    <button type="button" onClick={action('Button 2')}>
+      Button 2
+    </button>
+    <button type="button" onClick={action('Button 3')}>
+      Button 3
+    </button>
+    <button type="button" onClick={action('Button 4')}>
+      Button 4
+    </button>
   </ButtonGroup>
 );
 storiesOf('ButtonGroup', module).add('Three Button Alt', () => (
