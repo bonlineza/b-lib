ba7d6d6521770c925a775bd2ba63272c9cac8794
Musango Wope <musangow@gmail.com>
2019-11-05 12:05:01 +0200

    stories/HorizontalProgress: add two new stories,
    one is showcasing transition timeout
    and the other is showcasing the bar being adjustable

--- stories/HorizontalProgressBar/index.js
+++ stories/HorizontalProgressBar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import './OverrideStyle.scss';
 import HorizontalProgressBar from '../../src/components/HorizontalProgressBar/index.js';
@@ -10,11 +10,33 @@ storiesOf('HorizontalProgressBar', module).add('default', () => (
 ));
 
 storiesOf('HorizontalProgressBar', module).add('half way progress', () => (
-  <WrappedProgressBar totalProgress={100} currentProgress={50} />
+  <WrappedProgressBar progressPercentage={50} />
 ));
 
 storiesOf('HorizontalProgressBar', module).add('override style', () => (
   <div className="override-progress-bar">
-    <WrappedProgressBar totalProgress={100} currentProgress={50} />
+    <WrappedProgressBar progressPercentage={50} />
   </div>
 ));
+
+storiesOf('HorizontalProgressBar', module).add(
+  'timeout of 100ms for transition',
+  () => <WrappedProgressBar progressPercentage={40} transitionTimeout={100} />,
+);
+
+const AdjustableProgressBar = () => {
+  const [barIsFull, setBarIsFullState] = useState(false);
+  return (
+    <Fragment>
+      <WrappedProgressBar progressPercentage={barIsFull ? 100 : 0} />
+      <button type="button" onClick={() => setBarIsFullState(!barIsFull)}>
+        {barIsFull ? 'Empty Bar' : 'Fill Bar'}
+      </button>
+    </Fragment>
+  );
+};
+
+storiesOf('HorizontalProgressBar', module).add(
+  'Button controlling progess',
+  () => <AdjustableProgressBar />,
+);
