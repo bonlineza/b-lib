68e7de24b3836ad2ce583db1798e3f1eef1ac578
Musango Wope <musangow@gmail.com>
2019-11-19 16:48:26 +0200

    add doc blocks for SimpleSelect and create md in docs dir

---
+++ docs/SimpleSelect.md
@@ -0,0 +1,9 @@
+# SimpleSelect
+
+## Props
+| Name            | Type     | Default     | Required | Description                                                         |
+| --------------- | -------- | ----------- | -------- | ------------------------------------------------------------------- |
+| onChange        | Function | () => false | false    |                                                                     |
+| value           | any      | null        | false    | value passed into `value`  prop of `SimpleSelect`                   |
+| options         | Array    | []          | false    | Array of object with value `{ label, value }`. Selection of options |
+| hideResetButton | boolean  | true        | false    | If true, hides reset button to clear value                          |
--- src/components/SimpleSelect/index.js
+++ src/components/SimpleSelect/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import { SimpleSelect as ReactSelect } from 'react-selectize';
 import './scss/SimpleSelect.scss';
 
-const SimpleSelect = ({ onChange, value, options, hideResetButton = true }) => (
+type PropShapes = {
+  onChange?: Function,
+  /** value passed into `value`  prop of `SimpleSelect` */
+  value?: any,
+  /** Array of object with value `{ label, value }`. Selection of options */
+  options?: Array,
+  /** If true, hides reset button to clear value */
+  hideResetButton?: boolean,
+};
+
+const SimpleSelect = ({
+  onChange,
+  value,
+  options,
+  hideResetButton,
+}: PropShapes) => (
   <ReactSelect
     hideResetButton={hideResetButton}
     onValueChange={onChange}
@@ -10,5 +25,12 @@ const SimpleSelect = ({ onChange, value, options, hideResetButton = true }) => (
     options={options}
   />
 );
+
+SimpleSelect.defaultProps = {
+  onChange: () => false,
+  value: null,
+  options: [],
+  hideResetButton: true,
+};
 
 export default SimpleSelect;
