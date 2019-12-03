1ff1ea166f62a0f4a7b30c7ebdd6ef155e7e93c1
Sebastian Scholle <sscholle@gmail.com>
2019-12-03 10:13:07 +0200

    Add Updated storybook build with Readme addon

---
+++ docs/storybook/precache-manifest.b18e2a4e34cc1b67d11ad5e5937260d9.js
@@ -0,0 +1,23 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2eb7dbd93d9bcfd051c5e7cbed6f7b0",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.24241a57e81a5f7e7dbb.bundle.js"
+  },
+  {
+    "revision": "fd06c7d63c0b57c13d00",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.24241a57e81a5f7e7dbb.bundle.js"
+  },
+  {
+    "url": "vendors~main.24241a57e81a5f7e7dbb.bundle.js"
+  },
+  {
+    "revision": "28246d4a05f6b136023a",
+    "url": "vendors~main.css"
+  }
+]);
