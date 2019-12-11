f5e4efd84f13ee2cbb02711033b88cb89c2abfd1
Musango Wope <musangow@gmail.com>
2019-12-11 16:27:15 +0200

    Add debounce util with js doc

---
+++ src/util/debounced.js
@@ -0,0 +1,22 @@
+/**
+ * function that enforces programming practice used to ensure that
+ * time-consuming task do not fire so often, that it stalls the performance
+ * of the web page. Limiting the rate at which a function gets invoked.
+ *
+ *
+ * @return function
+ * @param delay
+ */
+
+export function debounced(delay) {
+  let timer = null;
+  return function(fn, ...args) {
+    const context = this;
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      fn.apply(context, args);
+    }, delay);
+  };
+}
