7cadc2d060d7f4830cd3a35da0c69bbab5fad5ae
Musango Wope <musangow@gmail.com>
2019-12-11 16:27:38 +0200

    Add hasValue function with test and js description

---
+++ src/util/hasValue.js
@@ -0,0 +1,10 @@
+/**
+ * Function that accepts data and determines whether that data has
+ * value or not
+ *
+ *
+ * @return boolean
+ * @param value
+ */
+
+export default value => value !== null && value !== undefined && value !== '';
