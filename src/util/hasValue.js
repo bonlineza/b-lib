d5ba3625da5db9c49dbb2ce63a1aae398282054a
Musango Wope <musangow@gmail.com>
2019-12-12 13:39:30 +0200

    hasValue: fix description
    update description

--- src/util/hasValue.js
+++ src/util/hasValue.js
@@ -1,7 +1,7 @@
 /**
- * ensures that the value passed in is not null or undefined or is not an empty
+ * Checks if a value is not null, undefined or an empty string
  * string
  * @param {any} value: this checked to see whether it has a value
- * @return boolean
+ * @returns boolean
  */
 export default value => value !== null && value !== undefined && value !== '';
