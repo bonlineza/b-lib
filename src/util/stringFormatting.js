4c5de1da52347a48afd59f477c8daa2d0d60a87c
Musango Wope <musangow@gmail.com>
2019-12-12 12:01:15 +0200

    stringFormatting: refactor descriptions and reorder

--- src/util/stringFormatting.js
+++ src/util/stringFormatting.js
@@ -1,19 +1,17 @@
 /**
  * Function that accepts string and replaces underscores with spaces
- *
- * @return string
  * @param {string} str: the string that the transformation happens on
  * turning underscore into spaces
+ * @return string
  */
 export function replaceUnderscoreWithSpace(str) {
   return str.replace(/_/g, ' ');
 }
 /**
  * Function that accepts string and capitalizes first character of every word
- *
- * @return string
  * @param {string} str: the first character of each word is capitalized for
  * this string
+ * @return string
  */
 export function toTitleCase(str) {
   return str.replace(
@@ -23,12 +21,11 @@ export function toTitleCase(str) {
 }
 
 /**
- * Function that accepts string and replaces underscores with spaces and
- * capitalizes (the first character of every word uppercased)
- *
- * @return boolean
- * @param {string} str: string for which transformation happens for capitalizes
- * string
+ * Function that accepts string then removes underscores and capitalizes
+ * @param {string} str: string which undergoes two modifications 1)
+ * replacing underscores with spaces and capitalizing the first character of
+ * each word
+ * @return string
  */
 export function snakeCaseToTitleCase(str) {
   return toTitleCase(replaceUnderscoreWithSpace(str));
