c08330621a17b21ba5bc64e54de2ea81ff09ee78
Musango Wope <musangow@gmail.com>
2019-12-12 11:58:14 +0200

    hasValue: refactor description of function

--- src/util/hasValue.js
+++ src/util/hasValue.js
@@ -1,5 +1,6 @@
 /**
- * ensures that the value passed in is not null or undefined
+ * ensures that the value passed in is not null or undefined or is not an empty
+ * string
  * @param {any} value: this checked to see whether it has a value
  * @return boolean
  */
