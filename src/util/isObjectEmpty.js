915d11f4f0014bb7e7c686a00e51fa1b6846e658
Musango Wope <musangow@gmail.com>
2019-12-12 12:00:29 +0200

    isobjectEmpty: fix ordering params and description

--- src/util/isObjectEmpty.js
+++ src/util/isObjectEmpty.js
@@ -2,7 +2,7 @@
  * Function that accepts object as parameter and checks whether
  * Object is empty or not
  * @param {object} obj: the object that is checked to see whether it has
- * @return boolean
  * any keys
+ * @return boolean
  */
 export default (obj = {}) => !Object.keys(obj).length;
